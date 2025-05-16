1ebc3cdb74db206e77b9d385e75b069e09cc5e76
Artem Simutin <artemsimutin@gmail.com>
2025-05-16 21:58:33 +0300

    ✨ (prettierignore): add .prettierignore file to exclude globals.css from formatting ⬆️ (package.json): upgrade tailwindcss dependency to version 4.1.7 for new features and improvements ♻️ (globals.css): refactor CSS to use CSS variables for better maintainability and readability 📝 (page.tsx): update Home component structure for improved layout ♻️ (footer.tsx): refactor footer component for better layout and styling consistency ♻️ (button.tsx): update button component styles to improve accessibility and visual consistency
    ♻️ (tailwind.config.cjs): remove Tailwind CSS configuration file to streamline project dependencies and reduce complexity

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,19 +2,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full">
+    <footer className="w-full flex flex-col">
       {/* decoration */}
       <div></div>
+
       {/* main content */}
-      <div></div>
+      <div className="max-w-container-desktop px-container-desktop"></div>
+
       {/* sub footer */}
-      <div className="w-full flex justify-between items-center mb-6xl mt-8xl">
+      <div className="w-full flex justify-between items-center pb-6xl pt-8xl border-border-secondary">
         <span className="text-text-quaternary">
           © 2025 Dawn Studio. All rights reserved.
         </span>
         {/* icons */}
-        <div className="flex">
-          <span>x</span>
+        <div className="flex gap-x-3xl">
+          <span className="text">x</span>
           <span>a</span>
         </div>
       </div>
