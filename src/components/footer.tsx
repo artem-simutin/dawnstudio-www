5795fb5e09b7f3bdf1c00d7b20de89903804fbb2
Artem Simutin <artemsimutin@gmail.com>
2025-05-16 19:55:55 +0300

    ✨ (package.json): upgrade @tailwindcss/postcss to version 4.1.7 for improved features ✨ (package.json): upgrade tailwindcss to version 3.4.17 for better performance ♻️ (globals.css): refactor Tailwind CSS imports to use @tailwind directive for better clarity ♻️ (page.tsx): refactor Home component to use Footer component for cleaner structure ♻️ (footer.tsx): refactor footer layout and styling for improved design consistency ♻️ (tailwind.config.cjs): refactor spacing configuration for better readability and maintainability

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,8 +8,15 @@ const Footer = () => {
       {/* main content */}
       <div></div>
       {/* sub footer */}
-      <div className="w-full flex justify-between items-center">
-        <span className="text">© 2025 Dawn Studio. All rights reserved.</span>
+      <div className="w-full flex justify-between items-center mb-6xl mt-8xl">
+        <span className="text-text-quaternary">
+          © 2025 Dawn Studio. All rights reserved.
+        </span>
+        {/* icons */}
+        <div className="flex">
+          <span>x</span>
+          <span>a</span>
+        </div>
       </div>
     </footer>
   );
